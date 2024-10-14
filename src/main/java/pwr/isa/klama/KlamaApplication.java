be84a13582ec8ea46303599a9f67801c899323cf
Adrian Goral <adrian.goral@gmail.com>
2024-10-14 20:01:48 +0200

    initial commit

---
+++ src/main/java/pwr/isa/klama/KlamaApplication.java
@@ -0,0 +1,13 @@
+package pwr.isa.klama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pwr/isa/klama/KlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.isa.klama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
