e02708ecaf7ed3b7563a8e76b2a51164b57279b9
Adrian Goral <adrian.goral@gmail.com>
2024-10-22 01:48:10 +0200

    Added custom exception

--- src/main/java/pwr/isa/klama/security/config/CustomExceptionHandler.java
+++ src/main/java/pwr/isa/klama/security/config/CustomExceptionHandler.java
@@ -2,35 +2,40 @@ package pwr.isa.klama.security.config;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import pwr.isa.klama.exceptions.ResourceNotFoundException;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex, WebRequest request) {
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        String message = ex.getMessage();
-        String errorFromHttp = HttpStatus.BAD_REQUEST.getReasonPhrase();
-        String requestUri = request.getDescription(false);
-        String errorJson = "{\"timestamp\":\"" + timestamp + "\",\"message\":\"" + message + "\",\"error\":\"" + errorFromHttp + "\",\"status\":\"" + HttpStatus.BAD_REQUEST.value() + "\",\"uri\":\"" + requestUri + "\"}";
-        return new ResponseEntity<>(errorJson, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, Object>> handleIllegalStateException(IllegalStateException ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", new Timestamp(new Date().getTime()));
+        response.put("message", ex.getMessage());
+        response.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("uri", request.getDescription(false).substring(4));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        String message = ex.getMessage();
-        String errorFromHttp = HttpStatus.NOT_FOUND.getReasonPhrase();
-        String requestUri = request.getDescription(false);
-        String errorJson = "{\"timestamp\":\"" + timestamp + "\",\"message\":\"" + message + "\",\"error\":\"" + errorFromHttp + "\",\"status\":\"" + HttpStatus.NOT_FOUND.value() + "\",\"uri\":\"" + requestUri + "\"}";
-        return new ResponseEntity<>(errorJson, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", new Timestamp(new Date().getTime()));
+        response.put("message", ex.getMessage());
+        response.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("uri", request.getDescription(false).substring(4));
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
     // Add other exception handlers as needed
 }
