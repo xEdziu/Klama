7451c75b76866455b54db92f1a58659fc8480b0a
Adrian Goral <adrian.goral@gmail.com>
2024-10-26 21:15:39 +0200

    Revert "Resolved issue with FK blocking delete of user"
    This reverts commit 5c1696cbdd332f332a33bbc06626681a27184c0b.

--- src/main/java/pwr/isa/klama/posts/Post.java
+++ src/main/java/pwr/isa/klama/posts/Post.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import pwr.isa.klama.user.User;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.sql.Timestamp;
 
@@ -32,7 +30,6 @@ public class Post {
             nullable = false,
             name = "author_id"
     )
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private User authorId;
     private java.sql.Timestamp createdAt;
     private java.sql.Timestamp updatedAt;
